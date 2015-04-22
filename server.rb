dc944d0f34dab7973d3a206a50fa7d3372e6f680
Carlos Socorro <carlosrsocorro@gmail.com>
2015-04-22 12:00:42 -0400

    first heroku commit

---
+++ server.rb
@@ -0,0 +1,46 @@
+#server.rb
+
+require 'sinatra'
+
+get '/' do 
+"Hello World"
+end
+
+get '/blah' do 
+	"Hello blah!!"
+
+end
+
+get '/sinatra' do 
+	"Hello sinatra"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
